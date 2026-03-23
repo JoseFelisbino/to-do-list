ad33bcca0bf3b4fbacf4f2c234cb41a2c55eab46
FelisbinoJose <246760.jose@unibave.net>
2026-03-23 02:10:35 -0300

    feat: implementa consumo da api e renderização de tarefas

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,1 +1,33 @@
-console.log("Frontend iniciado");
+const API = "https://to-do-list-1-5ou2.onrender.com/tarefas";
+
+const lista = document.getElementById("lista-tarefas");
+
+async function carregarTarefas() {
+    try {
+        const response = await fetch(API);
+        const tarefas = await response.json();
+
+        renderizarTarefas(tarefas);
+    } catch (erro) {
+        console.error("Erro ao carregar tarefas:", erro);
+    }
+}
+
+function renderizarTarefas(tarefas) {
+    lista.innerHTML = "";
+
+    tarefas.forEach(tarefa => {
+        const li = document.createElement("li");
+
+        li.innerHTML = `
+            <strong>${tarefa.titulo}</strong><br>
+            <small>${tarefa.descricao || ""}</small><br>
+            <small>${tarefa.dataPrevista}</small><br>
+            <small>Status: ${tarefa.status}</small>
+        `;
+
+        lista.appendChild(li);
+    });
+}
+
+carregarTarefas();
